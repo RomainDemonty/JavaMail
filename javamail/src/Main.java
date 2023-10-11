c1479f12cc4ca517a48c86c1aceb9dd5476f3116
CedGav19 <cedric.gavage@student.hepl.be>
2023-10-11 14:37:12 +0200

    tout debut du projet

--- javamail/src/Main.java
+++ javamail/src/Main.java
@@ -1,5 +1,9 @@
+import Swing.MiniOutlook;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        MiniOutlook MO = new MiniOutlook();
+        MO.setVisible(true);
     }
 }
--- javamail/src/Swing/MiniOutlook.java
+++ javamail/src/Swing/MiniOutlook.java
@@ -1,13 +1,53 @@
 package Swing;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class MiniOutlook {
-    private JTabbedPane tabbedPane1;
+public class MiniOutlook extends JFrame implements ActionListener {
+    private JTabbedPane menu;
     private JPanel panel1;
-    private JTextPane textPane1;
-    private JTextField textField1;
-    private JTextField textField2;
+    private JTextPane texte;
+    private JTextField destinataire;
+    private JTextField objet;
     private JButton ajouterUnePièceJointeButton;
     private JButton envoyerButton;
+
+    public MiniOutlook() {
+        panel1.setVisible(true);
+        setContentPane(panel1);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setTitle("MiniOutlook");
+        setSize(800,600);
+
+        ajouterUnePièceJointeButton.addActionListener(this);
+        envoyerButton.addActionListener(this);
+
+        // configurer la liaison avec le smails 
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if(e.getSource()==ajouterUnePièceJointeButton)
+        {
+            ajouterPieceJointe();
+        }
+        if(e.getSource()==envoyerButton)
+        {
+            envoyer();
+        }
+    }
+    public void ajouterPieceJointe() {
+        System.out.println("hello piece jointe");
+    }
+    public void envoyer() {
+        System.out.println("test d'envois");
+        System.out.println("destinataire = "+ destinataire.getText());
+        System.out.println("objet = "+ objet.getText());
+        System.out.println("text = "+ texte.getText());
+
+        // ici que on va utiliser le systeme  mail et envoyer
+    }
+
 }
+
