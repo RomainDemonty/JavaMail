515c16298aa49a80308ee0ed4b257c9007812cfb
RomainDemonty <romain.demonty@student.hepl.be>
2023-10-11 13:36:52 +0200

    debut java mail

---
+++ javamail/src/Swing/MiniOutlook.java
@@ -0,0 +1,13 @@
+package Swing;
+
+import javax.swing.*;
+
+public class MiniOutlook {
+    private JTabbedPane tabbedPane1;
+    private JPanel panel1;
+    private JTextPane textPane1;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JButton ajouterUnePièceJointeButton;
+    private JButton envoyerButton;
+}
